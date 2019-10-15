a8b82756244de2ea4a99eff771afa42a57fa66f0
Allen Sitho <AllenSitho@gmail.com>
2019-10-15 14:14:06 -0700

    Add project files.

---
+++ TestMSTeam1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestMSTeam1.Models;
+using Serilog;
+
+namespace TestMSTeam1.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger logger;
+
+		public HomeController(ILogger logger)
+		{
+			this.logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			logger.Error("Error!");
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ TestMSTeam1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace TestMSTeam1
+{
+	public class Program
+	{
+		//public static void Main(string[] args)
+		//{
+		//	CreateWebHostBuilder(args).Build().Run();
+		//}
+
+		//public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+		//	WebHost.CreateDefaultBuilder(args)
+		//		.UseStartup<Startup>();
+
+		public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+			.AddEnvironmentVariables()
+			.Build();
+
+		public static IWebHost BuildWebHost(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.UseSerilog()
+				.UseKestrel()
+				.Build();
+
+		public static void Main(string[] args)
+		{
+			Log.Logger = new LoggerConfiguration()
+				.ReadFrom.Configuration(Configuration)
+				.Enrich.WithProperty("App Name", "Serilog Web App Sample")
+				.CreateLogger();
+			try
+			{
+				BuildWebHost(args).Run();
+				return;
+			}
+			catch (Exception ex)
+			{
+				Log.Fatal(ex, "Host terminated unexpectedly");
+				return;
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}
+		}
+	}
+}
+
